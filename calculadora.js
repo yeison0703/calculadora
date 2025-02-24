1be0900392be78f98a04a2e5d1eac08252902da6
yeison0703 <yeisongallego2002@gmail.com>
2025-02-24 13:58:15 -0500

    primer commit

---
+++ calculadora.js
@@ -0,0 +1,32 @@
+let display = document.getElementById("display");
+
+function agregarValor (valor){
+    display.value += valor;
+}
+
+function limpiar(){
+    display.value = " ";
+}
+
+function calcular(){
+    if (display.value.trim() === "")
+        return;
+    try{
+        display.value = eval(display.value);
+    } catch{
+        display.value = "Error";
+    }
+}
+
+document.querySelectorAll(".botones button").forEach(boton =>{
+    boton.addEventListener("click", function(){
+        let valor = this.innerText;
+        if (valor === "C"){
+            limpiar();
+        } else if(valor === "="){
+            calcular();
+        } else{
+            agregarValor(valor);
+        }
+    });
+});
